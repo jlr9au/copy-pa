6bfead1eba206a5f44a315e38778e22057fcac09
Jamie Ryan <jlr9au@virginia.edu>
2020-09-15 11:34:00 -0400

    fixed migration I had changed

--- polls/migrations/0001_initial.py
+++ polls/migrations/0001_initial.py
@@ -29,12 +29,5 @@ class Migration(migrations.Migration):
                 ('question', models.ForeignKey(on_delete=django.db.models.deletion.CASCADE, to='polls.question')),
             ],
         ),
-        migrations.CreateModel(
-            name='Comments',
-            fields=[
-                ('id', models.AutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
-                ('name', models.CharField(max_length=200)),
-                ('body', models.TextField()),
-            ],
-        ),
+        
     ]
