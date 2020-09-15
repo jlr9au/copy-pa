a4b7e605f9e84b16b7263f1688632210dddb2d0c
Jamie Ryan <jlr9au@virginia.edu>
2020-09-14 20:55:50 -0400

    first commit

---
+++ polls/models.py
@@ -0,0 +1,37 @@
+import datetime
+from django.db import models
+from django.utils import timezone
+from django import forms
+from bootstrap4.widgets import RadioSelectButtonGroup
+
+# Create your models here.
+
+class Question(models.Model):
+    question_text = models.CharField(max_length=200)
+    pub_date = models.DateTimeField('date published')
+    def __str__(self):
+        return self.question_text
+    def was_published_recently(self):
+        now = timezone.now()
+        return now - datetime.timedelta(days=1) <= self.pub_date <= now
+    was_published_recently.admin_order_field = 'pub_date'
+    was_published_recently.boolean = True
+    was_published_recently.short_description = 'Published recently?'
+
+class Choice(models.Model):
+    question = models.ForeignKey(Question, on_delete=models.CASCADE)
+    choice_text = models.CharField(max_length=200)
+    votes = models.IntegerField(default=0)
+    def __str__(self):
+        return self.choice_text
+
+class Comments(models.Model):
+    name= models.CharField(null=False, max_length=200)
+    body= models.TextField()
+    def __str__(self):
+        return self.body
+    def postcomment(self):
+        self.save()
+    
+
+    
